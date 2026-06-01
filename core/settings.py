d8d68591ceb8790b0094b7b2d1014d3bda2e73ac
Lenard Büsing <buelenard@tutanota.com>
2026-06-01 13:14:36 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.environ.get('STATIC_FILES')
 
 # Django Rest Framework
 # https://www.django-rest-framework.org/
